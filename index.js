d61b26b8294ebfae66b07f3c2af4101acfa35705
Craig Lyons <craig@carbonfive.com>
2015-11-13 18:04:13 -0500

    Cleanup anonymous logic & front-end

--- index.js
+++ index.js
@@ -8,33 +8,35 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-var users = {};
-var numUsers = 0;
-
 io.on('connection', function(socket){
     socket.broadcast.emit('logon message', 'new user connected.');
-    var user_id = users.count + 1;
 
     socket.on('chat message', function(message){
-        var data = [socket.nickname, message];
+        var data = [get_nickname(), message];
         io.emit('chat message', data);
     });
 
     socket.on('set nickname', function(nickname){
-        previous = socket.nickname ? socket.nickname : 'Anonymous';
+        previous = get_nickname();
         socket.nickname = nickname;
-        users[nickname] = nickname;
-        ++numUsers;
         var data = {
             'previous': previous,
             'new': nickname
         }
-        socket.broadcast.emit('nickname change', data);
+        io.emit('nickname change', data);
     });
 
     socket.on('disconnect', function(){
         socket.broadcast.emit('logoff message', 'user disconnected.');
     });
+
+    function get_nickname(){
+        if(!socket.nickname) {
+            return 'Anonymous';
+        } else {
+            return socket.nickname;
+        }
+    }
 });
 
 http.listen(port, function(){

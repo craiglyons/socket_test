fe8d4bccd44bd493ecf3ecbfa1c2c4fabf646a84
Craig Lyons <craig@carbonfive.com>
2015-11-13 16:58:07 -0500

    initial demo commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = 4000;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var users = {};
+var numUsers = 0;
+
+io.on('connection', function(socket){
+    socket.broadcast.emit('logon message', 'new user connected.');
+    var user_id = users.count + 1;
+
+    socket.on('chat message', function(message){
+        var data = [socket.nickname, message];
+        socket.broadcast.emit('chat message', data);
+    });
+
+    socket.on('set nickname', function(nickname){
+        previous = socket.nickname;
+        socket.nickname = nickname;
+        users[nickname] = nickname;
+        ++numUsers;
+        var data = {
+            'previous': previous,
+            'new': nickname
+        }
+        io.emit('nickname change', data);
+    });
+
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('logoff message', 'user disconnected.');
+    });
+});
+
+http.listen(port, function(){
+    console.log("listening on *" + port);
+});
